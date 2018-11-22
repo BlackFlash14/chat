7a2c0f2ada6a2f0a28ce4672db01a38d87ebb62e
abylkasymovkuat <abylkasymov.kuat@gmail.com>
2018-11-22 11:29:40 +0600

    first_commit

---
+++ chat/local_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chat',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# 'TEST': {
+#     'NAME': 'test_db',
+# },
